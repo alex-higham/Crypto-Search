ee44e6e5a6292b02513ff2c8f05b553a595b7ada
alex-higham <highama@oregonstate.edu>
2023-11-18 20:32:05 -0800

    Cleaned up server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const dotenv = require('dotenv');
 const axios = require('axios');
 
-// Midleware and MongoDB
+// Midleware
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 3001;
